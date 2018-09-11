7f5668e4e7e6c75c10c407b9dd49c068aa331900
sungmin414 <sungmin3231@gmail.com>
2018-09-11 21:49:17 +0900

    bookmark url, 제네릭views 설정

--- app/bookmark/views.py
+++ app/bookmark/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView, DetailView
+
+from .models import Bookmark
+
+
+# list_view
+class BookmarkLV(ListView):
+    model = Bookmark
+
+
+# detail_view
+class BookmarkDV(DetailView):
+    model = Bookmark
+
