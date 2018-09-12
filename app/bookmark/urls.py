6e6eff99d008e193148e8c006ebadb731de5e9f1
sungmin414 <sungmin3231@gmail.com>
2018-09-12 20:13:48 +0900

    url 설정(다른연결방법은 주석처리) 및 템플릿작성(list, detail)

--- app/bookmark/urls.py
+++ app/bookmark/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
+from django.views.generic import ListView, DetailView
 
+from .models import Bookmark
 from . import views
 
 app_name = 'bookmark'
 
 urlpatterns = [
     path('', views.BookmarkLV.as_view(), name='index'),
+    # path('', ListView.as_view(model=Bookmark), name='index'),
     path('<int:pk>/', views.BookmarkDV.as_view(), name='detail'),
+    # path('<int:pk>/', DetailView.as_view(model=Bookmark), name='detail')
 ]
