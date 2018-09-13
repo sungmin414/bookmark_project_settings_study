a8f59e3420669c5cf442fafb0f8e892a9313118e
sungmin414 <sungmin3231@gmail.com>
2018-09-13 18:53:53 +0900

    admin 수정

--- app/blog/admin.py
+++ app/blog/admin.py
@@ -5,9 +5,13 @@ from .models import Post
 
 
 class PostAdmin(admin.ModelAdmin):
+    # post 객체를 보여줄때 title, modify_date를 화면에 출력하도록 지정
     list_display = ('title', 'modify_date')
+    # modify_date 컬럼을 사용하는 필터 사이드바를 보여주도록 지정
     list_filter = ('modify_date', )
+    # title, content 컬럼에서 검색하도록 지정
     search_fields = ('title', 'content')
+    # title 필드를 사용해 미리 채워지도록 지정
     prepopulated_fields = {'slug': ('title',)}
 
 
