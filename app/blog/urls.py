953dba2e93fac93277a26372302108140aab5ec1
sungmin414 <sungmin3231@gmail.com>
2018-09-14 20:04:11 +0900

    views수정, urls 구성

---
+++ app/blog/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from .views import PostLV, PostDV, PostAV, PostYAV, PostDAV, PostTAV
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('', PostLV.as_view(), name='index'),
+    path('post/', PostLV.as_view(), name='post_list'),
+    path('post/<slug>/', PostDV.as_view(), name='post_detail'),
+    path('archive/', PostAV.as_view(), name='post_archive'),
+    path('<int:year>/', PostYAV.as_view(), name='post_year_archive'),
+    path('<int:year>/<str:month>/<int:day>/', PostDAV.as_view(), name='post_day_archive'),
+    path('today/', PostTAV.as_view(), name='post_today_archive'),
+
+]
--- app/blog/views.py
+++ app/blog/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView, DetailView, ArchiveIndexView, YearArchiveView, MonthArchiveView, \
+    DayArchiveView, TodayArchiveView
+
+from .models import Post
+
+
+class PostLV(ListView):
+    model = Post
+    template_name = 'blog/post_all.html'
+    context_object_name = 'posts'
+    paginate_by = 2
+
+
+class PostDV(DetailView):
+    model = Post
+
+
+class PostAV(ArchiveIndexView):
+    model = Post
+    date_field = 'modify_date'
+
+
+class PostYAV(YearArchiveView):
+    model = Post
+    date_field = 'modify_date'
+    make_object_list = True
+
+
+class PostMAV(MonthArchiveView):
+    model = Post
+    date_field = 'modify_date'
+
+
+class PostDAV(DayArchiveView):
+    model = Post
+    date_field = 'modify_date'
+
+
+class PostTAV(TodayArchiveView):
+    model = Post
+    date_field = 'modify_date'
+
+
